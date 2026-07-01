9597a590d5a9c020a8ed27cb76515c29746d4907
jalfairouz <431107828@qu.edu.sa>
2026-07-01 17:02:02 +0300

    "Implement Unit of Work pattern and configure initial database migration"

--- HelpDeskTickets/Program.cs
+++ HelpDeskTickets/Program.cs
@@ -1,3 +1,7 @@
+using HelpDeskTickets.EF;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    b=> b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+    ));
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
